7161110a9a7061b5c256807108437e9fcd055232
Nel <01169201@pw.edu.pl>
2023-11-04 13:14:33 +0100

    Jacoco as test coverage tool
    Added Jacoco plugin for test coverage.
    Added 1 method and 2 tests to check configuration.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Is 3 even? - " + isNumberEven(3));
     }
+
+    public static boolean isNumberEven(int number){return number%2==0;}
+
 }
--- src/test/java/MySimpleTest.java
+++ src/test/java/MySimpleTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+
+import static org.example.Main.isNumberEven;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MySimpleTest {
@@ -6,4 +8,12 @@ public class MySimpleTest {
     void testAdd() {
         assertEquals(5, 5, "Test to check if JUnit works fine.");
     }
+
+    @Test
+    void Number4IsEven() {
+        assertTrue(isNumberEven(4), "Test to check if isNumberEven return true for 4");}
+
+    @Test
+    void Number5IsOdd() {
+        assertFalse(isNumberEven(5), "Test to check if isNumberEven return false for 5");}
 }
