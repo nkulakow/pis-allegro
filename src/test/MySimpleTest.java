4e85b0cd5f7070d6d09f981487eaacfbdd5c740d
Marcin <01169256@pw.edu.pl>
2023-11-04 10:50:21 +0100

    Configured JUnit
    Added JUnit plugin to pom.xml. Wrote a simple exemplary test.

---
+++ src/test/MySimpleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MySimpleTest {
+    @Test
+    void testAdd() {
+        assertEquals(5, 5, "Test to check if JUnit works fine.");
+    }
+}
