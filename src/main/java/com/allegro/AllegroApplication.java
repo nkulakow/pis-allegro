cb0792a3c3d3ea157dbfc27a4ebc0dca608b152f
Nel <01169201@pw.edu.pl>
2023-12-07 16:49:47 +0100

    stuff

--- src/main/java/com/allegro/AllegroApplication.java
+++ src/main/java/com/allegro/AllegroApplication.java
@@ -2,11 +2,12 @@ package com.allegro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-@SpringBootApplication
-@RestController
+import org.springframework.stereotype.Controller;
+@Controller
 public class AllegroApplication {
 
     public static void main(String[] args) {
@@ -14,8 +15,10 @@ public class AllegroApplication {
     }
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "number", defaultValue = "1") int num) {
-        return"Hello! Is given number (" + num + ") even: " + isNumberEven(num);
+    public String sayHello(@RequestParam(value = "number", defaultValue = "1") int num, Model model) {
+        model.addAttribute("num", num);
+        model.addAttribute("even", isNumberEven(num));
+        return "hello";
     }
 
     public static boolean isNumberEven(int number){return number%2==0;}
