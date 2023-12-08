0950974b35e33bde1332677dd8f1a016052c19f8
Nel <01169201@pw.edu.pl>
2023-12-08 21:19:41 +0100

    cleaning

--- src/main/java/com/allegro/Controllers/AllegroController.java
+++ src/main/java/com/allegro/Controllers/AllegroController.java
@@ -46,14 +46,6 @@ public class AllegroController {
         return modelAndView;
     }
 
-    static boolean preFlightChecks(MongoClient mongoClient) {
-        Document pingCommand = new Document("ping", 1);
-        Document response = mongoClient.getDatabase("admin").runCommand(pingCommand);
-        System.out.println("=> Print result of the '{ping: 1}' command.");
-        System.out.println(response.toJson(JsonWriterSettings.builder().indent(true).build()));
-        return response.getDouble("ok").equals(1.0);
-    }
-
     public static boolean isNumberEven(int number){return number%2==0;}
 
 }
